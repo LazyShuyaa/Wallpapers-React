6eb9fce46450b669a0c849a6ae247eafabc716f6
LazyShuyaa <168578401+LazyShuyaa@users.noreply.github.com>
2024-05-19 23:06:23 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,40 +11,39 @@ app.use(express.json());
 async function scrapeWallpapersFromUrl(url) {
   const base_url = "https://wallpapers.com";
 
-  axios.get(url)
-    .then(response => {
-      const data = [];
+  try {
+    const response = await axios.get(url);
+    const data = [];
 
-      if (response.status === 200) {
-        const $ = cheerio.load(response.data);
-        const picture_tags = $('picture').slice(2);
+    if (response.status === 200) {
+      const $ = cheerio.load(response.data);
+      const picture_tags = $('picture').slice(2);
 
-        picture_tags.each((index, element) => {
-          const source_tag = $(element).find('source').first();
-          const img_tag = $(element).find('img').first();
+      picture_tags.each((index, element) => {
+        const source_tag = $(element).find('source').first();
+        const img_tag = $(element).find('img').first();
 
-          const thumbnail_url = source_tag.attr('data-srcset');
-          const img_url = img_tag.attr('data-src');
-          const alt = img_tag.attr('alt');
+        const thumbnail_url = source_tag.attr('data-srcset');
+        const img_url = img_tag.attr('data-src');
+        const alt = img_tag.attr('alt');
 
-          if (thumbnail_url && img_url && !img_url.endsWith('.svg')) {
-            const imgUrl = new URL(img_url, base_url).href;
-            const thumbnailUrl = new URL(thumbnail_url, base_url).href;
+        if (thumbnail_url && img_url && !img_url.endsWith('.svg')) {
+          const imgUrl = new URL(img_url, base_url).href;
+          const thumbnailUrl = new URL(thumbnail_url, base_url).href;
 
-            data.push({
-              caption: alt || '',
-              image_url: imgUrl,
-              thumbnail_url: thumbnailUrl
-            });
-          }
-        });
-      }
+          data.push({
+            caption: alt || '',
+            image_url: imgUrl,
+            thumbnail_url: thumbnailUrl
+          });
+        }
+      });
+    }
 
-      return data;
-    })
-    .catch(error => {
-      throw new Error(`Error scraping wallpapers: ${error.message}`);
-    });
+    return data;
+  } catch (error) {
+    throw new Error(`Error scraping wallpapers: ${error.message}`);
+  }
 }
 
 async function scrapeWallpapers(searchTerm, page = 1) {
